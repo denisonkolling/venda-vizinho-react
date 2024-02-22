697b79de32d50e23416c642b7ae91a9200f6f2e6
Denison Kolling <denisonkolling@gmail.com>
2024-02-22 20:28:26 -0300

    refactor: header router links update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,9 @@
-//Utilities
-import { useNavigate } from 'react-router-dom'
-
-//Styles
+import { Link } from 'react-router-dom'
 import { Navbar, Nav, Container, NavbarBrand } from 'react-bootstrap';
 import { FaHeart, FaUser, FaBagShopping } from 'react-icons/fa6';
 import logo from '../assets/logo.svg';
 
 const Header = () => {
-
-
-	const navigate = useNavigate()
-
-	const handleLogoClick = () => {
-    navigate('/')
-  }
-
-  const handleProductsClick = () => {
-    navigate('/produtos')
-  }
-
-
-	const handleCategoriesClick = () => {
-    navigate('/categorias')
-  }
-
-	const handleOffersClick = () => {
-    navigate('/ofertas')
-  }
-
-	const handleLoginClick = () => {
-    navigate('/login')
-  }
-
-  const handleSignUpClick = () => {
-    navigate('/sign-up')
-  }
-
-  const handleSignOutClick = () => {
-    // dispatch(logoutUser())
-    // signOut(auth)
-  }
-
-  const handleCartClick = () => {
-    // dispatch(toggleCart())
-  }
-
 
 	return (
 		<>
@@ -52,11 +11,11 @@ const Header = () => {
 				<Container
 					className="container-fluid justify-content-center"
 					style={{ letterSpacing: '0.5px' }}>
-					<NavbarBrand>
-						<img src={logo} alt="" width="60" height="37" onClick={handleLogoClick}/>
+					<NavbarBrand as={Link} to='/home'>
+						<img src={logo} alt="" width="60" height="37"/>
 					</NavbarBrand>
 					<Navbar.Brand
-						href="/home"
+						as={Link} to='/home'
 						className="text-white fw-bold"
 						style={{ letterSpacing: '1px' }}>
 						Venda de Vizinho
@@ -64,25 +23,25 @@ const Header = () => {
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 						<Nav className="me-auto">
-							<Nav.Link onClick={handleProductsClick} className="text-white fw-medium">
+							<Nav.Link as={Link} to='/produtos' className="text-white fw-medium">
 								Produtos
 							</Nav.Link>
-							<Nav.Link onClick={handleCategoriesClick} className="text-white fw-medium">
+							<Nav.Link as={Link} to='/categorias' className="text-white fw-medium">
 								Categorias
 							</Nav.Link>
-							<Nav.Link onClick={handleOffersClick} className="text-white fw-medium">
+							<Nav.Link as={Link} to='/ofertas' className="text-white fw-medium">
 								Ofertas
 							</Nav.Link>
 						</Nav>
 					</Navbar.Collapse>
 					<div className="d-flex">
-						<Nav.Link className="text-reset m-3">
+						<Nav.Link as={Link} to='/favoritos' className="text-reset m-3">
 							<FaHeart className="text-white" />
 						</Nav.Link>
-						<Nav.Link className="text-reset m-3">
+						<Nav.Link as={Link} to='/carrinho' className="text-reset m-3">
 							<FaBagShopping className="text-white" />
 						</Nav.Link>
-						<Nav.Link className="text-reset m-3">
+						<Nav.Link as={Link} to='/login' className="text-reset m-3">
 							<FaUser className="text-white" />
 						</Nav.Link>
 					</div>
